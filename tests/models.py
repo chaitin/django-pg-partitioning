4e74c094dcfd481732f3634298b74f0a6118c191
monouno <xingji2163@163.com>
2019-02-10 17:18:51 +0800

    Format code

--- tests/models.py
+++ tests/models.py
@@ -1,39 +1,30 @@
 from django.contrib.postgres.indexes import BrinIndex
 from django.db import models
 from django.utils import timezone
-
 from pg_timepart.constants import PeriodType
 from pg_timepart.manager import TimeRangePartitioningSupport
 
 
-@TimeRangePartitioningSupport(partition_key="timestamp",
-                              default_period=PeriodType.Month,
-                              default_attach_tablespace="data1",
-                              default_detach_tablespace="data2")
+@TimeRangePartitioningSupport(
+    partition_key="timestamp", default_period=PeriodType.Month, default_attach_tablespace="data1", default_detach_tablespace="data2"
+)
 class ExampleModel1(models.Model):
     text = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now)
 
     class Meta:
         db_tablespace = "pg_default"
-        indexes = [
-            BrinIndex(fields=["timestamp"])
-        ]
+        indexes = [BrinIndex(fields=["timestamp"])]
         unique_together = ("text", "timestamp")
         ordering = ["text"]
 
 
-@TimeRangePartitioningSupport(partition_key="timestamp",
-                              default_period=PeriodType.Day,
-                              default_attach_tablespace="data2",
-                              default_detach_tablespace="data1")
+@TimeRangePartitioningSupport(partition_key="timestamp", default_period=PeriodType.Day, default_attach_tablespace="data2", default_detach_tablespace="data1")
 class ExampleModel2(models.Model):
     text = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now)
 
     class Meta:
-        indexes = [
-            BrinIndex(fields=["timestamp"])
-        ]
+        indexes = [BrinIndex(fields=["timestamp"])]
         unique_together = ("text", "timestamp")
         ordering = ["text"]
